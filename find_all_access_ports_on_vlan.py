d3bb31a34261f44c1f27e7b481e3a4f8e87dbb93
OrnateCaesar <david_lawson@outlook.com>
2022-10-26 15:04:38 +0100

    Added find_all_access_ports_on_vlan.py

---
+++ find_all_access_ports_on_vlan.py
@@ -0,0 +1,57 @@
+import os
+from pprint import pprint
+import meraki
+from meraki_utils import connect_to_meraki, meraki_error, other_error
+
+
+API_KEY = os.getenv('MERAKI_API_KEY')
+vlan_of_interest = '999'
+
+dashboard = connect_to_meraki(API_KEY)
+
+print(f"Getting organizations...")
+try:
+    organizations = dashboard.organizations.getOrganizations()
+except meraki.APIError as e:
+    meraki_error(e)
+    exit(1)
+except Exception as e:
+    other_error(e)
+    exit(1)
+
+print(f"Obtaining networks for organization: {organizations[0]['name']}")
+try:
+    networks = dashboard.organizations.getOrganizationNetworks(organizationId=organizations[0]['id'])
+except meraki.APIError as e:
+    meraki_error(e)
+    exit(1)
+except Exception as e:
+    other_error(e)
+    exit(1)
+
+# Remove networks without switches
+networks = [n for n in networks if 'switch' in n['productTypes']]
+
+for network in networks:
+    print(f"Obtaining switches in network: {network['name']}")
+    try:
+        switches = dashboard.networks.getNetworkDevices(networkId=network['id'])
+    except meraki.APIError as e:
+        meraki_error(e)
+        exit(1)
+    except Exception as e:
+        other_error(e)
+        exit(1)
+
+    for switch in switches:
+        print(f"Obtaining ports on switch \'{switch['name']}\' in network \'{network['name']}\'")
+        try:
+            ports = dashboard.switch.getDeviceSwitchPorts(serial=switch['serial'])
+        except meraki.APIError as e:
+            meraki_error(e)
+            exit(1)
+        except Exception as e:
+            other_error(e)
+            exit(1)
+
+
